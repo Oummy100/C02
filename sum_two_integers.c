e8394b6451d78e4008ad6e877f8f558746a95398
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-27 13:50:58 +0100

    exo 2

---
+++ sum_two_integers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, somme;
+
+    // Demande de saisir deux entiers
+    printf("Entrez deux entiers : ");
+    scanf("%d %d", &a, &b);
+
+    // Calcul de la somme
+    somme = a + b;
+
+    // Affichage du résultat
+    printf("La somme est : %d\n", somme);
+
+    return 0;
+}
